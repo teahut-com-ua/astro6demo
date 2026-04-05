a26c48ab58868601325cfaa20467a494458fdd26
Oleksandr Kulkov <hutofsato@gmail.com>
2026-04-05 15:26:53 +0300

    Initial comment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
+import icon from 'astro-icon';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://example.com',
+    integrations: [mdx(), icon(), sitemap()],
+});

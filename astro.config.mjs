963344e0e561823d195221868fe9e964a9d5961b
Oleksandr Kulkov <hutofsato@gmail.com>
2026-05-06 20:47:41 +0300

    Changed Search

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
-
 import icon from 'astro-icon';
-
 import sitemap from '@astrojs/sitemap';
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
     security: { csp: true },
-    integrations: [mdx(), icon(), sitemap()],
+    integrations: [mdx(), icon(), sitemap(), pagefind()],
     image: {
         service: {
             entrypoint: 'astro/assets/services/sharp',
